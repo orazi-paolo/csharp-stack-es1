db83d54fae0ac8f0767b73549a9b42e27950e819
orazi-paolo <orazi.paolo10@gmail.com>
2024-12-29 20:12:27 +0100

    first commit

---
+++ csharp-stack-es1/Program.cs
@@ -0,0 +1,13 @@
+﻿// dichiaro uno stack di stringhe
+var stack = new Stack<string>(new[] { "Ciao", "Mondo", "Come", "Stai" });
+
+// tolgo l'ultimo elemento inserito
+stack.Pop();
+
+// stampo lo stack aggiornato
+Console.WriteLine("Stack aggiornato:");
+
+foreach (string item in stack)
+{
+    Console.WriteLine(item);
+}
